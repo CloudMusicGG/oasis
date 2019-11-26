4d8363e4ce8bfa8886c666d16ee029cfaa8c52a7
pelo_rosa <pelo_rosa@163.com>
2019-11-26 11:09:54 +0800

    初始化项目配置

---
+++ src/main.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: rosalee
+ * @Date: 2019-11-26 10:25:50
+ * @LastEditors: rosalee
+ * @LastEditTime: 2019-11-26 10:58:24
+ * @Description: 
+ */
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios';
+import ElementUI from 'element-ui';
+import MintUI from 'mint-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'mint-ui/lib/style.css'
+import store from './store'
+
+Vue.use(ElementUI);
+Vue.use(MintUI);
+
+Axios.defaults.baseURL = '/api/'; //把axios的请求基本路径改为api，以后就不需要在每个axios请求里写api
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: rosalee
+ * @Date: 2019-11-26 10:57:24
+ * @LastEditors: rosalee
+ * @LastEditTime: 2019-11-26 10:57:56
+ * @Description: 
+ */
+
+import Vue from 'vue'
+import VueX from 'vueX'
+
+Vue.use(VueX)
+
+export default new VueX.Store({
+    state: {//存储数据
+        Tel:"123",
+        isLogin:false
+    },
+    mutations: {//跟踪状态
+        changeTel(state,tel){
+            state.Tel = tel;
+        },
+        changeLogin(state,param){
+            state.isLogin = param;
+        }
+    },
+    actions: {//有异步请求，异步请求完成后，提交mutations
+
+    }
+})
